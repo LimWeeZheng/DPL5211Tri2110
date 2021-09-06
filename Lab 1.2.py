5ca2a6a607c9d575996464c35e782155f13b6f24
LimWeeZheng <90165799+LimWeeZheng@users.noreply.github.com>
2021-09-06 10:42:16 +0800

    Add files via upload

---
+++ Lab 1.2.py
@@ -0,0 +1,10 @@
+# Get input 2 numbers and display the sum of 2 numebrs
+
+num1 = int(input("First Number  : "))
+num2 = int(input("Second number : "))
+total = num1 + num2
+
+# print("Sum of both number : ",total)
+# Display --> The sum of num1 and num2 is total
+print("The sum of",num1,"and",num2,"is",total)
+print("The sum of {} and {} is {}".format(num1,num2,total))
