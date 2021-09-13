5d5907eb6f007a659052520781c0194bb6137d43
LimWeeZheng <90165799+LimWeeZheng@users.noreply.github.com>
2021-09-13 09:42:57 +0800

    Add files via upload

---
+++ Lab 2.1.py
@@ -0,0 +1,6 @@
+# Student ID: 1201200463
+# Student Name: Lim Wee Zheng
+
+kilo = float(input("Please enter the value of kilogram: "))
+gram = kilo * 1000
+print("For {:.2f} kilogram is equivalent to {:.2f} gram".format(kilo,gram))
---
+++ Lab 2.2.py
@@ -0,0 +1,12 @@
+# Student ID: 1201200463
+# Student Name: Lim Wee Zheng
+import math
+
+FT = 0.3048 #constant value, the value doesn't change, name in Capital letters
+
+feet = float(input("Please enter the value of feet: "))
+meters = FT * feet
+
+print("{:.2f} feet is equivalent to {:.2f} meters".format(feet,meters))
+print(math.pi)
+print(math.e)
